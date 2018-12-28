1c2e88bc6534816f046188ca67ff9967bc1dabb7
renfaizal <ren.faizal@gmail.com>
2018-12-28 13:03:27 +0700

    update

--- app/src/main/java/com/example/renfa/kostin/profile.java
+++ app/src/main/java/com/example/renfa/kostin/profile.java
@@ -3,11 +3,11 @@ package com.example.renfa.kostin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class kamar extends AppCompatActivity {
+public class profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kamar);
+        setContentView(R.layout.activity_profile);
     }
 }
